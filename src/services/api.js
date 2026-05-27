4e8e1276c870d7741d4a36428d1a5817dfef745c
noraaranara <anastasiya.solntseva@gmail.com>
2026-05-27 22:01:53 +0300

    finished hw-5

---
+++ src/services/api.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+
+const API_URL = 'https://wedev-api.sky.pro/api/kanban';
+
+export async function fetchCard({ token }) {
+  try {
+    const data = await axios.get(API_URL, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+    return data.data.cards;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function postCard({ token, card }) {
+  try {
+    const data = await axios.post(API_URL, card, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': '',
+      },
+    });
+    return data.data.cards;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function getCard({ token, id }) {
+  try {
+    const data = await axios.get(API_URL + id, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    });
+    return data.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function editCard({ token, id, card }) {
+  try {
+    const data = await axios.put(API_URL + id, card, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': '',
+      },
+    });
+    return data.data.cards;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function deleteCard({ token, id }) {
+  try {
+    const data = await axios.put(API_URL + id, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'text',
+      },
+    });
+    return data.data.cards;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
